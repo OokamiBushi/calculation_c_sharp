60473c885a2c38300087ba0b93fc75983ddac98f
Sergey Volkov <volk44296@gmail.com>
2025-12-04 21:40:55 +0300

    commit1

--- Program.cs
+++ Program.cs
@@ -1,17 +1,36 @@
 ﻿Console.WriteLine("Введите сумму вашего счета: ");
-double invoiceAmount = double.Parse(Console.ReadLine());//ввести сумму счета
-Console.WriteLine("Введите процент чаевых: ");
-double tipPercentage = double.Parse(Console.ReadLine());//ввести процент чаевых
-Console.WriteLine("Введите кол-во людей: ");
-int groupSize = int.Parse(Console.ReadLine());//ввести кол-во людей в группе
-
-double tipAmount = (invoiceAmount * tipPercentage) / 100;
-Console.WriteLine($"Процент чаевых составляет: {tipPercentage}%.");
-double totalAmount = invoiceAmount + tipAmount;
-Console.WriteLine($"Общая сумма счета составляет: {totalAmount}.");
-double equalAmount = totalAmount / groupSize;
-Console.WriteLine($"С каждого: {equalAmount}.");
-
+string invoiceAmount_str = Console.ReadLine();//ввести сумму счета
+if (double.TryParse(invoiceAmount_str, out double invoiceAmount))
+{
+    Console.WriteLine("Введите процент чаевых: ");
+}
+else
+{
+    Console.WriteLine("Ошибка запустите программу заново!!!");
+}
+string tipPercentage_str = Console.ReadLine();//ввести процент чаевых
+if (double.TryParse(tipPercentage_str, out double tipPercentage))
+{
+    Console.WriteLine("Введите кол-во людей: ");
+}
+else
+{
+    Console.WriteLine("Ошибка запустите программу заново!!!");
+}
+string groupSize_str = Console.ReadLine();//ввести кол-во людей в группе
+if (double.TryParse(groupSize_str, out double groupSize))
+{
+    double tipAmount = (invoiceAmount * tipPercentage) / 100;
+    Console.WriteLine($"Процент чаевых составляет: {tipPercentage}%.");
+    double totalAmount = invoiceAmount + tipAmount;
+    Console.WriteLine($"Общая сумма счета составляет: {totalAmount}.");
+    double equalAmount = totalAmount / groupSize;
+    Console.WriteLine($"С каждого: {equalAmount}.");
+}
+else
+{
+    Console.WriteLine("Ошибка запустите программу заново!!!");
+}
 
 
 
